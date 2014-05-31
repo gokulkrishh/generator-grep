2dd4c403846ebe9f2846a2969d471ab45c877c37
gokulkrishh <krishnangokul9@gmail.com>
2014-05-31 23:49:03 +0530

    initial version 0.0.1

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
-var chalk = require('chalk');
+
+//dependencies
+var util    = require('util'),
+    path    = require('path'),
+    yeoman  = require('yeoman-generator'),
+    yosay   = require('yosay'),
+    chalk   = require('chalk');
+
 
 
 var GrepGenerator = yeoman.generators.Base.extend({
@@ -12,7 +15,7 @@ var GrepGenerator = yeoman.generators.Base.extend({
 
     this.on('end', function () {
       if (!this.options['skip-install']) {
-        this.installDependencies();
+        this.installDependencies(); //will install both  bower and npm dependencies
       }
     });
   },
@@ -21,33 +24,61 @@ var GrepGenerator = yeoman.generators.Base.extend({
     var done = this.async();
 
     // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Grep generator!'));
+    this.log(yosay('Welcome to this Awesome grep generator using Gulp.js'));
 
     var prompts = [{
       type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
+      name: 'Which one',
+      message: 'Would you like to proceed with AngularJS ?',
       default: true
+    },
+    {
+      type: 'checkbox',
+      name: 'Libraries',
+      message: 'Which libraries you would like to install ?',
+      choices : [{
+      	value   : 'jquery',
+      	name    : 'Jquery',
+      	checked : true
+      },
+      {
+      	value   : 'bootstrap',
+      	name    : 'Bootstrap',
+      	checked : false
+      }]
     }];
 
     this.prompt(prompts, function (props) {
       this.someOption = props.someOption;
-
       done();
     }.bind(this));
   },
 
   app: function () {
+    //development directory
     this.mkdir('app');
-    this.mkdir('app/templates');
-
+    this.mkdir('app/css');
+    this.mkdir('app/fonts');
+   	this.mkdir('app/js');    
+    this.mkdir('app/js/lib');
+    
+    //copying dependency files    
     this.copy('_package.json', 'package.json');
     this.copy('_bower.json', 'bower.json');
+    this.copy('_bowerrc', '.bowerrc');
+    this.copy('_gulpfile.js', 'gulpfile.js');
   },
 
   projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
+  	//copying project required files
+    this.copy('_editorconfig', '.editorconfig');
+    this.copy('_jshintrc', '.jshintrc');
+    this.copy('_gitignore', '.gitignore');
+
+    //copy the index.html, js & css files
+    this.copy('_index.html', 'app/index.html');
+    this.copy('_main.js', 'app/js/main.js');
+    this.copy('_styles.css', 'app/css/styles.css');
   }
 });
 
---
+++ app/templates/_gulpfile.js
@@ -0,0 +1,150 @@
+'use strict';
+
+var gulp 	  	= require('gulp'),
+	concatBower = require('gulp-bower-files'),
+	chalk 	  	= require('chalk'),
+	connect 	= require('connect'),
+	concat 	  	= require('gulp-concat'),
+	gulpif 	  	= require('gulp-if'),
+	jshint 	  	= require('gulp-jshint'),
+	livereload  = require('gulp-livereload'),
+	minifycss 	= require('gulp-minify-css'),
+	minifyhtml 	= require('gulp-minify-html'),
+	runSequence = require('run-sequence'),
+	stylish   	= require('jshint-stylish'),
+	server   	= require('tiny-lr'),
+	uglify    	= require('gulp-uglify'),
+	watch     	= require('gulp-watch');
+
+var src = {
+	root : 'app',
+	css : 'app/css',
+	js : 'app/js',
+	lib : 'app/lib',
+	images : 'app/images'
+};
+
+var	build = {
+	root : 'build',
+	css : 'build/css',
+	js : 'build/js',
+	lib : 'build/lib',
+	images : 'build/images'
+};
+
+var production = false;
+
+// minify html options
+var opts = {
+	comments: false, 
+	quotes: true, 
+	spare:true, 
+	empty: true, 
+	cdata:true
+};
+
+// chalk config
+var error  = chalk.red.bold,
+	hint   = chalk.yellow.bold,
+	change  = chalk.red;
+
+/**================================================
+  		Static server using connect and tiny-lr
+===================================================*/
+
+
+
+
+
+/**================================================
+  		HTML -- minify html to build
+===================================================*/
+
+gulp.task('html', function() {
+	console.log(hint('\n --------- Running html tasks --------------------------------------------->>>'));
+	return gulp.src(['app/*.html'])
+	.pipe(gulpif(production, minifyhtml(opts)))
+	.pipe(gulp.dest(build.root))
+	.pipe(livereload());
+});
+
+/**===============================================
+  		CSS Tasks -- minify, concat
+=================================================*/
+
+gulp.task('css', function() {
+	console.log(hint('\n --------- Running css tasks ---------------------------------------------->>>'));
+	return gulp.src(['app/css/*.css'])
+	.pipe(gulpif(production, minifycss()))
+	.pipe(concat('styles.css'))
+	.pipe(gulp.dest(build.css))
+	.pipe(livereload());
+});
+
+/**================================================
+  		Script Tasks -- js hint, uglify, concat
+===================================================*/
+
+gulp.task('scripts', function() {
+	console.log(hint('\n --------- Running script tasks ------------------------------------------->>>'));
+	return gulp.src(['app/js/**/*.js', ]) // 'gulpfile.js'
+	.pipe(jshint('.jshintrc'))
+	.pipe(jshint.reporter(stylish))
+	.pipe(concat('all.js'))
+	.pipe(gulpif(production, uglify()))
+	.pipe(gulp.dest(build.js))
+	.pipe(livereload());
+});
+
+/**======================================================
+  	Concat - all bower packages files to bower.js
+=========================================================*/
+
+gulp.task('concat-bower', function() {
+	concatBower().pipe(concat('bower.js'))
+	.pipe(gulpif(production, uglify()))
+	.pipe(gulp.dest(build.js));
+});
+
+/**===============================================
+  		Watch -- all files
+=================================================*/
+
+gulp.task('watch', function() {
+	console.log(hint('\n --------- Watching all files --------------------------------------------->>> \n'));
+	var html = gulp.watch(['app/js/*.html'], ['html']),
+		script = gulp.watch(['app/js/**/*.js'], ['scripts']),
+		css    = gulp.watch(['app/css/*.css'], ['css']);
+
+	var log = function(event) {
+		console.log(change('\n -- File ' + event.path + ' was ' + event.type + ' -->>> \n'));
+	}
+
+	//on change print file name and event type
+	html.on('change', log);
+	script.on('change', log);
+	css.on('change', log);
+
+});
+
+/**==============================================
+  		Gulp build Tasks - dev, production
+=================================================*/
+
+gulp.task('build', function() {
+	console.log(hint('\n --------- Build Development Mode  ---------------------------------------->>> \n'));
+	runSequence('html', 'scripts', 'css', 'concat-bower', 'watch');
+});
+
+gulp.task('prod', function() {
+	console.log(hint('\n --------- Build Production Mode  --------------------------------------------->>> \n'));
+	production = true;
+	runSequence('html', 'scripts', 'css', 'concat-bower', 'watch');
+});
+
+
+/**==============================================
+  		Gulp Default Tasks -- all
+=================================================*/
+
+gulp.task('default', ['build']);
